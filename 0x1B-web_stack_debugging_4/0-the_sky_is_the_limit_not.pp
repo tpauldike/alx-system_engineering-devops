e0d916dcb4a69797dbbe6b800ecf3779863050ba
tpauldike <topman4loveworld@gmail.com>
2023-06-16 01:35:05 +0100

    dix nginx to handle more traffic per time

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increase the amount of traffic that nginx can take per time
+
+exec { 'increase-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
